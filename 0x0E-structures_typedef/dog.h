8e824449d4149633c8dc108c8d4f9d700e99263b
Yiga Budalah <igabudalah@gmail.com>
2022-08-01 09:15:30 -0700

    my dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,28 @@
-#ifndef _DOG_
-#define _DOG_
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
-* struct dog - Dog with a few attributes
-* @name: The name of the dog as a pointer to the first character of a string
-* @age: The age of the dog aas a floa  
-* @owner: The name of the owner of the dog as a pointer to the first character
-* of a string
-*/
-typedef struct dog
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-char *dup_strint(char *s);
 void free_dog(dog_t *d);
-
 #endif
