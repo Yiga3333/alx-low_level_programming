07362de9e1688a1609821ffbf49799143e9a27f7
Yiga Budalah <igabudalah@gmail.com>
2022-08-22 16:20:48 -0700

    my first comit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
+@@ -0,0 +1,23 @@
 #include "lists.h"
 /**
- * print_listint - print list
- * Return: size_t
- * @h: list
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
-	int nodes = 0;
+	size_t nelem;
 
-	while (h)
+	nelem = 0;
+
+	while (h != NULL)
 	{
-		nodes++;
-		printf("%d", h->n);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		printf("\n");
+		nelem++;
 	}
-	return (nodes);
+	return (nelem);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,27 @@
-#include <unistd.h>
-
+#ifndef _LISTS_
+#define _LISTS_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-int _putchar(char c)
+typedef struct list_s
 {
-	return (write(1, &c, 1));
-}
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
