986234ad850e0b8bbafffc9454743b5ec703b513
Yiga Budalah <igabudalah@gmail.com>
2022-08-25 14:24:53 -0700

    my days

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,27 +1,13 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
+#include "function_pointers.h"
 /**
- * sum_them_all - Entry Point
- * @n: variadic inputs
- * Return: sum
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
  */
-int sum_them_all(const unsigned int n, ...)
+void print_name(char *name, void (*f)(char *))
 {
-	va_list valist;
-	unsigned int i, sum = 0;
-
-	if (n == 0)
-		return (0);
-
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(valist, const unsigned int);
-	}
-
-	va_end(valist);
-
-	return (sum);
+	if (name && f)
+		f(name);
 }
